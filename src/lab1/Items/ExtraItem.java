0ab01f75b662abffff712fd9c21cb0b818b10e88
khromachu <nadushok.khrom777@gmail.com>
2021-06-09 17:53:22 +0700

    Лабораторная 1

---
+++ src/lab1/Items/ExtraItem.java
@@ -0,0 +1,10 @@
+package lab1.Items;
+
+import lab1.Item;
+
+public class ExtraItem extends Item {
+    public ExtraItem(String name, Integer weight, Integer price)
+    {
+        super(name, weight, price);
+    }
+}
